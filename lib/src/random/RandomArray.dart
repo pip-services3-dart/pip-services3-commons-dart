672f5c530db5f9f9f59d2fbf9abf82ccdeee4d1e
Sergey Seroukhov <seroukhov@gmail.com>
2020-03-25 12:33:12 -0700

    Ported Random package

--- lib/src/random/RandomArray.dart
+++ lib/src/random/RandomArray.dart
@@ -1,25 +1,24 @@
-// /** @module random */
-// import { RandomInteger } from './RandomInteger';
+import './RandomInteger.dart';
 
-// /**
-//  * Random generator for array objects.
-//  * 
-//  * ### Example ###
-//  * 
-//  *     let value1 = RandomArray.pick([1, 2, 3, 4]); // Possible result: 3
-//  */
-// export class RandomArray {
+/**
+ * Random generator for array objects.
+ * 
+ * ### Example ###
+ * 
+ *     let value1 = RandomArray.pick([1, 2, 3, 4]); // Possible result: 3
+ */
+class RandomArray {
     
-//     /**
-//      * Picks a random element from specified array.
-//      * 
-//      * @param values    an array of any type
-//      * @returns         a randomly picked item.
-//      */
-//     public static pick<T>(values: T[]): T {
-//         if (values == null || values.length == 0)
-//             return null;
+    /**
+     * Picks a random element from specified array.
+     * 
+     * @param values    an array of any type
+     * @returns         a randomly picked item.
+     */
+    static T pick<T>(List<T> values) {
+        if (values == null || values.length == 0)
+            return null;
 
-//         return values[RandomInteger.nextInteger(values.length)];
-//     }
-// }
+        return values[RandomInteger.nextInteger(values.length)];
+    }
+}
