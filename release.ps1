34679711a12220fb1cf53d6aa7acae4d463f5598
Levichev Dmitry <levichev.dmitry@gmail.com>
2020-03-30 15:28:35 +0300

    Fix deploy scripts

--- release.ps1
+++ release.ps1
@@ -4,17 +4,17 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$version = (Get-Content -Path package.json | ConvertFrom-Json).version
+$version = (Get-Content -Path pubspec.yaml | ConvertFrom-Yaml).version
 
 if ($component.version -ne $version) {
-    throw "Versions in component.json and package.json do not match"
+    throw "Versions in component.json and pubspec.yaml do not match"
 }
 
 # Automatically login to server
-if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null) {
-    npm-cli-login
-}
+# if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null) {
+#     npm-cli-login
+# }
 
 # Publish to global repository
 Write-Output "Pushing package to npm registry"
-npm publish
+pub publish
